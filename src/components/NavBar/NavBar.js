d16f68433dab10a93dbfadd8b806c543ecc9a112
Federico Martin Badan <federicobadan@hotmail.com>
2021-06-03 02:13:42 -0300

    navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const NavBar = ()=>{
+
+    return <>
+    <nav className='navbar'>
+        <div className='navbar-divlogo'>
+            <img id="logo" src='../logo.png' alt='logo'></img>
+        </div>
+        <div className='navbar-divtexto'>
+            <a className='navbar-texto' href='index.html'>Inicio</a>
+            <a className='navbar-texto' href='index.html'>Categoria 1</a>
+            <a className='navbar-texto' href='index.html'>Categoria 2</a>
+            <a className='navbar-texto' href='index.html'>Contacto</a>
+        </div>
+    </nav>
+    </>
+}
