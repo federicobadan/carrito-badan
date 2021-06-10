0f1faf8f049ebd0ab9703a8d4f75739923925f97
Federico Martin Badan <federicobadan@hotmail.com>
2021-06-10 02:23:45 -0300

    fix class

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -4,7 +4,7 @@ export const CartWidget = ()=>{
 
     return <>
     <div className='navbar-cartwidget'>
-        <button className='btn-cartwidget'><i class="fa fa-shopping-cart"></i></button>
+        <button className='btn-cartwidget'><i className="fa fa-shopping-cart"></i></button>
     </div>
     </>
 }
