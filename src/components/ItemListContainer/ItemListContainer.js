ba844529f2e73f5b0d943503ad1e648cf369c8b3
Federico Martin Badan <federicobadan@hotmail.com>
2021-06-17 20:57:01 -0300

    correccion imagenes

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -2,6 +2,11 @@ import React, {useState, useEffect} from 'react';
 import './ItemListContainer.css';
 import { ItemList } from '../ItemList/ItemList.js';
 import imagen1 from './img/mouse/mouse.png';
+import imagen2 from './img/teclado/teclado.png';
+import imagen3 from './img/placas-de-video/placa-de-video.png';
+import imagen4 from './img/placas-madre/placa-madre.png';
+import imagen5 from './img/placas-de-sonido/placa-de-sonido.png'
+
 
     const myPromise = () => {
         return new Promise ((resolve, reject) => {
@@ -10,35 +15,35 @@ import imagen1 from './img/mouse/mouse.png';
                     {id:0, 
                     titulo: "Mouse Hyperx Pulsefire Core rgb HX-MC004B", 
                     precio:456, 
-                    img: {imagen1},
+                    img: `${imagen1}`,
                     stock: 20,
                     alt:'Mouse',
                     descripcion:'Mouse marca HyperX'},
                     {id:1, 
                     titulo: "Teclado Gamer Xtrike-me Gk-979 Qwerty Blue Inglés",
                     precio:1340,
-                    img:"../../screens/img/teclado/teclado.png",
+                    img: `${imagen2}`,
                     stock:25,
                     alt:'Teclado',
                     descripcion:'Teclado marca Xtrike'},
                     {id:2,
                     titulo: "Placa de Video Evga RTX 3070 XC3 Ultra 8Gb",
                     precio:76000,
-                    img:"../../screens/img/placas-de-video/placa-de-video.png",
+                    img:`${imagen3}`,
                     stock:30,
                     alt:'Placa de video',
                     descripcion:'Placa de video EVGA RTX 3070'},
                     {id:3,
                     titulo:"Motherboard Gigabyte S1151 H310M M2",
                     precio:58000,
-                    img:"../../screens/img/placas-madre/placa-madre.png",
+                    img: `${imagen4}`,
                     stock:35,
                     alt:'Motherboard',
                     descripcion:'Motherboard marca Gigabyte'},
                     {id:4,
                     titulo:"Placa de sonido Sound Blaster Audigy FX PCIe 5.1",
                     precio:20000,
-                    img:"../../screens/img/placas-de-sonido/placa-de-sonido.png",
+                    img: `${imagen5}`,
                     stock:40,
                     alt:'Placa de sonido',
                     descripcion:'Placa de sonido marca Sound Blaster'}
